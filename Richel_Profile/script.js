47c096caf3e75e91ec22ff0197ba406b112a7acc
RJhaine <21103788@usc.edu.ph>
2022-09-08 14:49:52 +0800

    added

---
+++ Richel_Profile/script.js
@@ -0,0 +1,138 @@
+// $(document).ready(function(){
+
+//   // alert("Document is ready!");
+
+// $('#myBtn').on('click', function () {
+//   var dots = document.getElementById("dots");
+//   var moreText = document.getElementById("more");
+//   var btnText = document.getElementById("myBtn");
+
+//   if (dots.style.display === "none") {
+//     dots.style.display = "inline";
+//     btnText.innerHTML = "Read more";
+//     $(moreText).hide('slow');
+//   } else {
+//     dots.style.display = "none";
+//     btnText.innerHTML = "Read less";
+//     $(moreText).show('slow');
+//   }
+// });
+// $('#myBtn1').on('click', function () {
+//   var dots = document.getElementById("dots");
+//   var moreText = document.getElementById("more1");
+//   var btnText = document.getElementById("myBtn1");
+
+//   if (dots.style.display === "none") {
+//     dots.style.display = "inline";
+//     btnText.innerHTML = "Read more";
+//     $(moreText).hide('slow');
+//   } else {
+//     dots.style.display = "none";
+//     btnText.innerHTML = "Read less";
+//     $(moreText).show('slow');
+//   }
+// });
+// $('#myBtnUN').on('click', function () {
+//   var dots = document.getElementById("dots");
+//   var moreText = document.getElementById("more2");
+//   var btnText = document.getElementById("myBtnUN");
+
+//   if (dots.style.display === "none") {
+//     dots.style.display = "inline";
+//     btnText.innerHTML = "See More";
+//     $(moreText).hide('slow');
+//   } else {
+//     dots.style.display = "none";
+//     btnText.innerHTML = "See Less";
+//     $(moreText).show('slow');
+//   }
+// });
+
+// $('#myBtnFord').on('click', function () {
+//   var dots = document.getElementById("dots");
+//   var moreText = document.getElementById("more3");
+//   var btnText = document.getElementById("myBtnFord");
+
+//   if (dots.style.display === "none") {
+//     dots.style.display = "inline";
+//     btnText.innerHTML = "See More";
+//     $(moreText).hide('slow');
+//   } else {
+//     dots.style.display = "none";
+//     btnText.innerHTML = "See Less";
+//     $(moreText).show('slow');
+//   }
+// });
+// $('#myBtnBM').on('click', function () {
+//   var dots = document.getElementById("dots");
+//   var moreText = document.getElementById("more4");
+//   var btnText = document.getElementById("myBtnBM");
+
+//   if (dots.style.display === "none") {
+//     dots.style.display = "inline";
+//     btnText.innerHTML = "See More";
+//     $(moreText).hide('slow');
+//   } else {
+//     dots.style.display = "none";
+//     btnText.innerHTML = "See Less";
+//     $(moreText).show('slow');
+//   }
+// });
+// $('#myBtnConrad').on('click', function () {
+//   var dots = document.getElementById("dots");
+//   var moreText = document.getElementById("more5");
+//   var btnText = document.getElementById("myBtnConrad");
+
+//   if (dots.style.display === "none") {
+//     dots.style.display = "inline";
+//     btnText.innerHTML = "See More";
+//     $(moreText).hide('slow');
+//   } else {
+//     dots.style.display = "none";
+//     btnText.innerHTML = "See Less";
+//     $(moreText).show('slow');
+//   }
+// });
+// });
+
+
+
+
+
+// // $('#toggleText()').on('click', function () {
+// //   var dots = document.getElementById("points");
+// //   var moreText = document.getElementById("moreText");
+// //   var btnText = document.getElementById("toggleText()");
+
+// //   if (dots.style.display === "none") {
+// //     dots.style.display = "inline";
+// //     btnText.innerHTML = "See More";
+// //     $(moreText).hide('slow');
+// //   } else {
+// //     dots.style.display = "none";
+// //     btnText.innerHTML = "See Less";
+// //     $(moreText).show('slow');
+// //   }
+// // });
+
+// // $('toggleText()'.on('click', function () {
+// //   var points =
+// //     document.getElementById("points");
+
+// //   var showMoreText =
+// //     document.getElementById("moreText");
+
+// //   var buttonText =
+// //     document.getElementById("textButton");
+// //   if 
+// //   (points.style.display === "none") {
+// //     showMoreText.style.display = "none";
+// //     points.style.display = "inline";
+// //     buttonText.innerHTML = "Show More";
+// //   }
+// //   else {
+// //     showMoreText.style.display = "inline";
+// //     points.style.display = "none";
+// //     $buttonText.innerHTML = "Show Less";
+// //   }
+// // }
